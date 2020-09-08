300f156759044cd0a94eb2e14d486f8ec222fee1
Sean Leavey <gpg@attackllama.com>
2020-09-08 22:07:37 +0200

    Add insert support

--- taransaydb/driver.py
+++ taransaydb/driver.py
@@ -3,6 +3,8 @@
 import os
 from enum import Flag, auto
 from pathlib import Path
+from shutil import copyfile
+from tempfile import NamedTemporaryFile
 from datetime import datetime, time, timedelta
 from collections.abc import Iterable, Reversible
 from .exceptions import ProgrammingError
@@ -19,7 +21,7 @@ class DriverAccessType(Flag):
         access_map = {
             cls.APPEND: "a",
             cls.READ: "r",
-            cls.WRITE: "w",
+            # Note: write mode is not used anywhere... yet.
         }
 
         return access_map[flag]
@@ -72,9 +74,73 @@ class DirectoryDriver:
                 f"{self} is not opened in a way that supports appending."
             )
 
-        line = [str(tick.time())] + self._format_data(data)
         fp = self._shard_stream(tick.date(), DriverAccessType.APPEND, create=True)
-        fp.write(" ".join(line) + "\n")
+        fp.write(self._format_line(tick, data))
+
+    def insert(self, tick, data):
+        """Insert data in order.
+
+        This assumes the data is already ordered.
+
+        If you know your `tick` is later than the last reading in the database, then
+        :class:`.append` is much quicker.
+        """
+        if DriverAccessType.WRITE not in self.access_type:
+            # Incorrect access type for this driver.
+            raise ProgrammingError(
+                f"{self} is not opened in a way that supports writing."
+            )
+
+        tick_date = tick.date()
+
+        fp_existing, fp_temp = self._shard_stream_with_tmp_buffer(
+            tick_date,
+            DriverAccessType.READ,  # Only read because we use a temporary write buffer instead.
+            create=True,
+        )
+
+        # Ensure buffered data is written.
+        fp_existing.flush()
+
+        insert_line = self._format_line(tick, data)
+        pivot_time = tick.time()
+        pivot_passed = False
+
+        for line in self._read_lines(fp_existing):
+            line_time, line_data = self._parse_line_time(line)
+
+            if line_time > pivot_time:
+                # The insert data should be inserted before this line.
+                fp_temp.file.write(insert_line)
+                pivot_passed = True
+
+            # Copy the existing line into the new file.
+            fp_temp.file.write(line + "\n")
+
+            if pivot_passed:
+                # Quickly write the rest of the chunks.
+                while True:
+                    data = fp_existing.read(1024)
+                    if not data:
+                        break
+                    fp_temp.file.write(data)
+
+                # We're done.
+                break
+
+        if not pivot_passed:
+            # The insert line is at the end.
+            fp_temp.file.write(insert_line)
+
+        # Substitute the shard with the temporary buffer.
+        self._shard_stream_replace_cache(
+            fp_existing, fp_temp, self._shard_path(tick_date)
+        )
+        # Delete the temporary file.
+        fp_temp.close()
+
+    def _format_line(self, tick, data):
+        return " ".join([str(tick.time())] + self._format_data(data)) + "\n"
 
     @property
     def path(self):
@@ -85,17 +151,11 @@ class DirectoryDriver:
             self.path / f"{date.year:04d}" / f"{date.month:02d}" / f"{date.day:02d}.txt"
         )
 
-    def _read_raw_lines(self, shard, reverse=False, buf_size=8192):
+    def _read_raw_lines(self, fp, reverse=False, buf_size=8192):
         """Memory-efficient, reversible line reader.
 
         Based on flyingcircus.readline.
         """
-        try:
-            fp = self._shard_stream(shard, DriverAccessType.READ)
-        except FileNotFoundError:
-            # No file, so nothing to read.
-            return
-
         is_bytes = isinstance(fp.read(0), bytes)
         newline = b"\n" if is_bytes else "\n"
         remainder = b"" if is_bytes else ""
@@ -141,20 +201,24 @@ class DirectoryDriver:
 
         yield remainder
 
-    def _parse_lines(self, shard_date, start, stop, reverse=False):
-        for lineno, line in enumerate(
-            self._read_raw_lines(shard_date, reverse=reverse), start=1
-        ):
+    def _read_lines(self, *args, **kwargs):
+        for line in self._read_raw_lines(*args, **kwargs):
             if not line or line.strip().startswith("#"):
                 # Skip empty or comment line.
                 continue
 
-            pieces = line.split()
+            yield line
 
-            assert len(pieces) >= 2
+    def _parse_lines(self, shard_date, start, stop, reverse=False):
+        try:
+            fp = self._shard_stream(shard_date, DriverAccessType.READ)
+        except FileNotFoundError:
+            # No file, so nothing to read.
+            return
 
+        for lineno, line in enumerate(self._read_lines(fp, reverse=reverse), start=1):
             try:
-                line_time = time.fromisoformat(f"{pieces[0]}")
+                line_time, line_data = self._parse_line_time(line)
             except ValueError as e:
                 if reverse:
                     e.args = (f"{e} (line -{lineno} of {self})",)
@@ -167,7 +231,13 @@ class DirectoryDriver:
 
             if (reverse and line_time < stop) or (not reverse and line_time >= start):
                 line_datetime = datetime.combine(shard_date, line_time)
-                yield [line_datetime] + self._parse_data(pieces[1:])
+                yield [line_datetime] + self._parse_data(line_data)
+
+    def _parse_line_time(self, line):
+        """Parse line time and return it along with the raw line data."""
+        pieces = line.split()
+        assert len(pieces) >= 2
+        return time.fromisoformat(f"{pieces[0]}"), pieces[1:]
 
     def _shard_stream(self, shard_date, mode=DriverAccessType.READ, create=False):
         if not self._is_open:
@@ -175,9 +245,6 @@ class DirectoryDriver:
                 f"{self} is not open. Data can only be queried when the driver is open and in read "
                 f"mode."
             )
-
-        # A mode check should already have been made by this point.
-        assert mode in self.access_type
 
         file_mode = DriverAccessType.file_mode(mode)
         shard_path = self._shard_path(shard_date)
@@ -196,6 +263,38 @@ class DirectoryDriver:
         self._file_cache[shard_path] = shard_path.open(file_mode)
 
         return self._file_cache[shard_path]
+
+    def _shard_stream_with_tmp_buffer(self, shard_date, *args, **kwargs):
+        """Get two shard streams for a given date: the real one, and a temporary buffer.
+
+        The real shard stream is the same one returned by :class:`._shard_stream`. The temporary one
+        has the same path and filename except with ".tmp" appended. The temporary buffer is
+        write-only. Once closed, the temporary file is deleted.
+        """
+        real_path = self._shard_path(shard_date)
+        fp_real = self._shard_stream(shard_date, *args, **kwargs)
+        fp_temp = NamedTemporaryFile(
+            mode="w", prefix=real_path.name, dir=str(real_path.parent)
+        )
+
+        return fp_real, fp_temp
+
+    def _shard_stream_replace_cache(self, fp_cached, fp_replacement, cached_path):
+        """Overwrite `fp_cached` file object's file with `fp_replacement` file object's contents."""
+        assert not fp_cached.closed
+        assert cached_path == Path(fp_cached.name)
+
+        fp_cached.close()
+
+        # Finish writing data.
+        fp_replacement.flush()
+
+        # Overwrite the original file's contents with that of the temporary file.
+        # This uses a memory-optimised copy operation starting from Python 3.8.
+        copyfile(fp_replacement.name, fp_cached.name)
+
+        # Re-open.
+        self._file_cache[cached_path] = cached_path.open(fp_cached.mode)
 
     def __str__(self):
         return f"{self.__class__.__name__}(access_type={self.access_type})"
---
+++ tests/test_insert.py
@@ -0,0 +1,19 @@
+from datetime import datetime, timedelta
+
+
+def test_insert(float_device, faker):
+    tick1 = datetime(2020, 2, 15, 11, 57, 35)
+    tick2 = datetime(2020, 2, 15, 12, 1, 20)
+    insert_datetime = datetime(2020, 2, 15, 12, 0, 5)
+    data = [faker.pyfloat(), faker.pyfloat()]
+
+    with float_device.writer() as driver:
+        driver.append(tick1, data)
+        driver.append(tick2, data)
+        driver.insert(insert_datetime, data)
+
+    with float_device.reader() as driver:
+        # Query to 1 second after so the data point is returned.
+        result = list(driver.query_interval(tick1, tick2 + timedelta(seconds=1)))
+
+    assert result == [[tick1] + data, [insert_datetime] + data, [tick2] + data]
